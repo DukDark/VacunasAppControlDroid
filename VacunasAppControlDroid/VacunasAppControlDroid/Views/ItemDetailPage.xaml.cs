0c1171d17f31b3cbcd938ab809953362197eca1b
pato_ <pato_@DESKTOP-IF3OGH8>
2024-02-26 00:46:36 -0500

    Agregar archivos de proyecto.

---
+++ VacunasAppControlDroid/VacunasAppControlDroid/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using VacunasAppControlDroid.ViewModels;
+using Xamarin.Forms;
+
+namespace VacunasAppControlDroid.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ VacunasAppControlDroid/VacunasAppControlDroid/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using VacunasAppControlDroid.Models;
+using VacunasAppControlDroid.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VacunasAppControlDroid.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
